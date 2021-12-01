798acabd76df56e4eb0391cdb07ca87de56e387e
DazComplexed <91510495+DazComplexed@users.noreply.github.com>
2021-12-01 18:44:56 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,121 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+var Obsticals 
+var cloud, cloudsGroup, cloudImage;
+var Obstical1
+var Obstical2
+var Obstical3
+var Obstical4
+var Obstical5
+var Obstical6
+var newImage;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+ 
+  Obstical1 = loadImage ("obstacle1.png")
+  Obstical2 = loadImage ("obstacle2.png")
+  Obstical3 = loadImage ("obstacle3.png")
+  Obstical4 = loadImage ("obstacle4.png")
+  Obstical5 = loadImage ("obstacle5.png")
+  Obstical6 = loadImage ("obstacle6.png")
+
+
+}
+
+function setup() {
+  createCanvas(600, 200);
+
+  
+  
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  // trex.addAnimation("collided",trex_collided)
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  console.log("Hello"+ 5)
+  
+}
+
+function draw() {
+  background(180);
+  
+  
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  
+  //spawn the clouds
+  spawnClouds();
+  
+  drawSprites();
+
+  spawnObsticals();
+}
+ 
+  function spawnObsticals() {
+  if (frameCount % 60 === 0) {
+ obsticals = createSprite(600,165,10,40)
+   obsticals.velocityX = -3
+   var rand = Math.round (random(1,6))
+   switch(rand) {
+     case 1: obsticals.addImage(Obstical1); break;
+     case 2: obsticals.addImage(Obstical2); break;
+     case 3: obsticals.addImage(Obstical3); break;
+     case 4: obsticals.addImage(Obstical4); break;
+     case 5: obsticals.addImage(Obstical5); break;
+     case 6: obsticals.addImage(Obstical6); break;
+
+   }
+  
+   obsticals.scale = 0.5
+   obstical.lifetime = 300
+  
+  
+  
+  }
+
+}
+
+
+
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    cloud = createSprite(600,100,40,10);
+    cloud.addImage(cloudImage)
+    cloud.y = Math.round(random(10,60))
+    cloud.scale = 0.4;
+    cloud.velocityX = -3;
+    
+    
+    //assigning lifetime to the variable
+    cloud.lifetime = 134
+    
+    //adjust the depth
+    cloud.depth = trex.depth
+    trex.depth = trex.depth + 1;
+    }
+}
+
